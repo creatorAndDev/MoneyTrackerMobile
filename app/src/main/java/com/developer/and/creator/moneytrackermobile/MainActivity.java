f78508c92a4dc609e06e19c57128b18f875bc680
Yuri <creator.and.dev@gmail.com>
2018-05-06 01:58:18 +0300

    +fixed

--- app/src/main/java/com/developer/and/creator/moneytrackermobile/MainActivity.java
+++ app/src/main/java/com/developer/and/creator/moneytrackermobile/MainActivity.java
@@ -5,7 +5,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,6 +42,18 @@ public class MainActivity extends AppCompatActivity {
         final Button add = (Button) findViewById(R.id.add);
         final ListView items = findViewById(R.id.items);
 
+        //
+        //animations
+//        final AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
+//        alphaAnimation.setDuration(1000);
+//
+//        final AnimationSet setAnimation = new AnimationSet(true);
+//        setAnimation.addAnimation(alphaAnimation);
+//
+//        setupAnimation(add, alphaAnimation, R.anim.alpha_anim);
+
+        //
+
         //создаем адаптер для ListView что бы передавать в него полученные данные
         final ItemsAdapter adapter = new ItemsAdapter();
         items.setAdapter(adapter);
@@ -55,6 +72,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    //animations
+//    private void setupAnimation(View view, final Animation animation, final int animationID) {
+//        view.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                AnimationUtils.loadAnimation(ViewAnimations.this.animationID) : animation);
+////                v.startAnimation(mChrckBox.isChecked) ?
+////                        AnimationUtils.loadAnimation(ViewAnimations.this.animationID) : animation);
+//            }
+//        });
+//    }
 
     private class ItemsAdapter extends ArrayAdapter<Item> {
         //передаем контенкст главного активити

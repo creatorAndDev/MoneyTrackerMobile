d79c950db4c46c66e5e247ba251bcadfff250487
Yuri <creator.and.dev@gmail.com>
2018-05-06 02:47:53 +0300

    +added xmls files for animate

--- app/src/main/java/com/developer/and/creator/moneytrackermobile/MainActivity.java
+++ app/src/main/java/com/developer/and/creator/moneytrackermobile/MainActivity.java
@@ -5,12 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,19 +35,7 @@ public class MainActivity extends AppCompatActivity {
         final EditText name = (EditText) findViewById(R.id.name);
         final EditText price = (EditText) findViewById(R.id.price);
         final Button add = (Button) findViewById(R.id.add);
-        final ListView items = findViewById(R.id.items);
-
-        //
-        //animations
-//        final AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
-//        alphaAnimation.setDuration(1000);
-//
-//        final AnimationSet setAnimation = new AnimationSet(true);
-//        setAnimation.addAnimation(alphaAnimation);
-//
-//        setupAnimation(add, alphaAnimation, R.anim.alpha_anim);
-
-        //
+        final ListView items = (ListView) findViewById(R.id.items);
 
         //создаем адаптер для ListView что бы передавать в него полученные данные
         final ItemsAdapter adapter = new ItemsAdapter();
@@ -66,23 +49,13 @@ public class MainActivity extends AppCompatActivity {
                     adapter.add(new Item(name.getText().toString(), Integer.valueOf(price.getText().toString())));
                     name.setText("");
                     price.setText("");
+
                 } catch (Exception e) {
                     //исключение, если поле является пустым
                 }
             }
         });
     }
-
-    //animations
-//    private void setupAnimation(View view, final Animation animation, final int animationID) {
-//        view.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                AnimationUtils.loadAnimation(ViewAnimations.this.animationID) : animation);
-////                v.startAnimation(mChrckBox.isChecked) ?
-////                        AnimationUtils.loadAnimation(ViewAnimations.this.animationID) : animation);
-//            }
-//        });
-//    }
 
     private class ItemsAdapter extends ArrayAdapter<Item> {
         //передаем контенкст главного активити
